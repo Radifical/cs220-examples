66d9b0c7a1a44a9093d71f140132c7ceaad91f70
Radif <59629676+Radifical@users.noreply.github.com>
2026-02-13 17:13:41 -0500

    Update bitOps.c

--- bitwise/bitOps.c
+++ bitwise/bitOps.c
@@ -3,6 +3,7 @@
 
 void setBit(unsigned long *val, int pos){
   // TODO: set the bit in pos 'pos' to 1
+  *val = *val | (1UL << pos)
 }
 
 void flipBit(unsigned long *val, int pos){
@@ -15,4 +16,7 @@ void clearBit(unsigned long *val, int pos){
 
 void flipBitFld(unsigned long *val, int len, int pos){
   // TODO: toggle the 'len' bits starting in position 'pos'
+  unsgined long msk = (1ul << len) - 1;
+  mask <= pos;
+  *val *= mask;
 }
